8080329ac1815d2b8530d4eb222b68c044c74a3a
Hongs <ihongs@live.cn>
2026-05-11 23:34:11 +0800

    更新 MCP

--- hongs-serv-magpie/src/main/java/io/github/ihongs/serv/magpie/mcp/McpAction.java
+++ hongs-serv-magpie/src/main/java/io/github/ihongs/serv/magpie/mcp/McpAction.java
@@ -33,7 +33,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.time.Duration;
 
 /**
  * MCP 服务接口
